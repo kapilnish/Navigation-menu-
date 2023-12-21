9e8f2aa1fa0191311a7a14e810488f4bd9b4d5d3
Kamlesh Nishad <91783684+kapilnish@users.noreply.github.com>
2023-12-21 11:58:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let list = document.querySelectorAll(".list");
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
